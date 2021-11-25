d74d3db7a3d8a4ff6be3957f3c277a977385817e
adamvergara <201710348@post.au.dk>
2021-11-25 15:49:02 +0100

    adding message module

---
+++ Messages.py
@@ -0,0 +1,29 @@
+import uuid
+
+# DIO messages
+class DIO:
+    # constructor
+    # has 4 variables: DAGRank, DAGID
+    def __init__(self, DAGRank: int):
+        self.DAGRank: int = DAGRank
+        self.DAGID: uuid = uuid.uuid4()
+
+    def get_rank(self) -> int:
+        return self.DAGRank
+
+    def get_ID(self) -> uuid:
+        return self.DAGID()
+
+# Dao messages
+class DAO:
+    # constructor
+    # has 4 variables: DAGRank, DAGID
+    def __init__(self, DAORank: int):
+        self.DAORank: int = DAORank                 # is the rank of the node issuing the message.
+        self.InstanceID: uuid = uuid.uuid4()
+
+    def get_rank(self) -> int:
+        return self.DAORank
+
+    def get_ID(self) -> uuid:
+        return self.InstanceID
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Network import Network
 import matplotlib.pyplot as plt
 import numpy as ns
 import random
-
+import simpy
 
 network = Network(50)
 
@@ -11,15 +11,16 @@ network = Network(50)
 xs = []
 ys = []
 for node in network.nodes:
-    xs.append(node.get_positionX())
+    xs.append(node.get_X())
     ys.append(node.get_Y())
     print("id: ", node.get_ID())
     print("rank: ", node.get_rank())
-    print(f"x position: {node.get_positionX()}    y position: {node.positionY}", )
+    print(f"x position: {node.get_X()}    y position: {node.get_Y}", )
     print("")
 
 
 
 plt.plot(xs,ys ,'ro')
 plt.show()
+
 
